a207d014d08662c2aaa2b6703b56dc5db0826b52
Andrew Ippoliti <acippolit@gmail.com>
2017-03-12 17:07:24 -0400

    Added 2 tabs to the shelf

--- js/svg-tools.js
+++ js/svg-tools.js
@@ -26,6 +26,32 @@ function makeSvgCodeForBox( x, y, w, h, cornerSize, cornerRadius ){
 	return '<g transform="translate('+x+','+y+')">'+paths.join(' ')+'</g>';
 }
 
+function makeSvgCodeForTab( x, y, w, h, cornerSize, cornerRadius ){
+	var paths = [];
+	var c = cornerSize;
+	var tl = 'M 0 0 L 0 '+cornerSize;
+	var bl = 'M 0 '+(h-cornerSize)+' L 0 '+h+' L '+cornerSize+' '+h;
+	var br = 'M '+(w-cornerSize)+' '+h+' L '+w+' '+h+' L '+w+' '+(h-cornerSize);
+	var tr = 'M '+w+' '+cornerSize+' L '+w+' 0';
+	// roundPathCorners(pathString, radius, useFractionalRadius)
+	if( cornerRadius ){
+		//tl = roundPathCorners( tl, cornerRadius, false );
+		bl = roundPathCorners( bl, cornerRadius, false );
+		br = roundPathCorners( br, cornerRadius, false );
+		//tr = roundPathCorners( tr, cornerRadius, false );
+	}
+	// top-left, left, bottom-left, bottom, bottom-right, right, top-right, top
+	paths.push('<path d="'+tl+'" ></path>');
+	paths.push('<path d="M 0 '+cornerSize+'L 0 '+(h-cornerSize)+'" ></path>');
+	paths.push('<path d="'+bl+'" ></path>');
+	paths.push('<path d="M '+cornerSize+' '+h+' L '+(w-cornerSize)+' '+h+'" ></path>');
+	paths.push('<path d="'+br+'" ></path>');
+	paths.push('<path d="M '+w+' '+(h-cornerSize)+' L '+w+' '+cornerSize+'" ></path>');
+	paths.push('<path d="'+tr+'"></path>');
+	//paths.push('<path d="M '+(w-cornerSize)+' 0 L '+cornerSize+' 0"></path>');
+	return '<g transform="translate('+x+','+y+')">'+paths.join(' ')+'</g>';
+}
+
 
 /* A note about SVG Scale and coordinates
  * http://wiki.inkscape.org/wiki/index.php/Units_In_Inkscape
